3ee1f6829d402915a1ee7f23b2f1ba5670ac041e
Anvi Gupta <165661045+hi-anvi@users.noreply.github.com>
2025-04-01 17:22:41 +0530

    Update basic-java.js

--- basic-java.js
+++ basic-java.js
@@ -1,6 +1,6 @@
 var centre = document.getElementById("centre");
 var disappear = function() {
   centre.style.display = "none";
-}
+};
 centre.addEventListener("animationend", disappear);
 centre.addEventListener("animationiteration", disappear);
