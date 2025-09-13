839137298eb8930f37089fe1a19ee924aac6c8ec
Anvi Gupta <anvigupta@Anvis-Laptop.local>
2025-09-13 20:45:29 +0530

    create basic app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from cs50 import SQL
+from flask import Flask, redirect, request, render_template
+from flask_session import Session
+
+# Create app
+app = Flask(__name__)
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Get sqlite database
+db = SQL("sqlite:///data.db")
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+app.route()
